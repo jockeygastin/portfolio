648a314350b28edd2f6db45016288352f228b959
jackie <jockeygastin1998@gmail.com>
2025-02-20 12:20:51 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental:{
+        turbo:{
+            resolveExtensions: [
+                ".ts",
+                ".tsx",
+                ".js",
+                ".jsx",
+                ".md",
+                ".mdx",
+                ".pdf"
+            ]
+        }
+    }
+};
 
 export default nextConfig;
