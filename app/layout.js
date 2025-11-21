cd10ef5ee1119aa3ed3ca696d87c01b37519dac7
jackie <jockeygastin1998@gmail.com>
2025-11-21 16:59:19 +0530

    og image

--- app/layout.js
+++ app/layout.js
@@ -11,10 +11,36 @@ const ovo = Ovo({
 });
 
 
+// export const metadata = {
+//   title: "Portfolio - Jockey gastin",
+//   description: "Jockey gastin portfolio",
+// };
+
 export const metadata = {
-  title: "Portfolio - Jockey gastin",
+  title: "Portfolio - Jockey Gastin",
   description: "Jockey gastin portfolio",
+  openGraph: {
+    title: "Portfolio - Jockey Gastin",
+    description: "Jockey gastin portfolio",
+    url: "https://portfolio-iota-wine-60.vercel.app/",
+    siteName: "Jockey Portfolio",
+    images: [
+      {
+        url: "https://portfolio-iota-wine-60.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portfolio - Jockey Gastin",
+    description: "Jockey gastin portfolio",
+    images: ["https://portfolio-iota-wine-60.vercel.app/og-image.png"],
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
